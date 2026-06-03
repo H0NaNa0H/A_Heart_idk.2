6961a82258b7e69c5031711864c8229d76acc534
H0NaNa0H <nadinegc.estud@gmail.com>
2026-06-03 13:29:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ else:
         time.sleep(0.3)
         st.markdown(
             '<p class="terminal-text" style="background-color: #1a0000; border: 1px solid #ff0055; padding: 15px; border-radius: 5px; font-weight: bold; color: #ff3366 !important;">'
-            '>> "Don\'t get used to this because I\'m just learning and I\'m not good at these things, let alone being \'remantic?\'"'
+            '>> "Don\'t get used to this cuz I\'m just learning and I\'m not good at these things, let alone being \'remantic?\'"'
             '</p>', 
             unsafe_allow_html=True
         )
